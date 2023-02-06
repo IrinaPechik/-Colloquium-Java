4900de6c1567b5dc56975e406aa711a5597156db
IrinaPechik <irinapechik@gmail.com>
2023-02-06 15:58:49 +0300

    Паттерн "Наблюдатель"

---
+++ untitled/src/main/java/org/example/JobSearch.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class JobSearch {
+    public static void main(String[] args) {
+        JavaDeveloperJobSite jobSite = new JavaDeveloperJobSite();
+        jobSite.addVacancy("First Java Position");
+        jobSite.addVacancy("Second Java Position");
+
+        Observer firstSubscriber = new Subscriber("Irina Pechik");
+        Observer secondSubscriber = new Subscriber("Ivan Ivanov");
+
+        jobSite.addObserver(firstSubscriber);
+        jobSite.addObserver(secondSubscriber);
+
+        jobSite.addVacancy("Third Java Position");
+        jobSite.removeVacancy("First Java Position");
+    }
+}
